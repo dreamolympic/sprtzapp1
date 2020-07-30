c1873e985f4aa47a0143aeef159a6730ab92514e
venkat <info@dreamolympic.co>
2020-07-30 12:49:44 +0530

    first version to upload to github.

--- app/src/main/java/com/example/sportzerplbschool/AcademyDetails.java
+++ app/src/main/java/com/example/sportzerplbschool/AcademyDetails.java
@@ -10,5 +10,6 @@ public class AcademyDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academy_details);
+        //test comment for git
     }
 }
